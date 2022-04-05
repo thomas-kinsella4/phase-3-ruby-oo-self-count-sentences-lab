463e21801b1aaa571123d7a94a576a8db1714826
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-04-05 16:50:34 -0400

    commit tree 395922867a1c8510dfac0d262b462740a93fb5b0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    # self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
